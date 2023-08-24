bdb0965f2fe5ed85dbea94550064b878e5a3513a
Mason Bassett <Bassettmason@gmail.com>
2023-08-24 17:41:06 +0000

    added post movie list functionality

--- main.py
+++ main.py
@@ -1,25 +1,47 @@
-from utils import remove_null_values, initialize_firestore, add_movie_to_firestore
+from utils import remove_null_values, initialize_firestore, add_movie_to_firestore, add_movie_list_to_firestore
 
 # Initializing Firestore client
 db = initialize_firestore()
 
-def add_movie_to_db(request):
-    """Adds a movie object to the Firestore database."""
+def movie_db(request):
+    """Adds a movie object or a movie list to the Firestore database."""
     if request.method != 'POST':
         return 'Only POST requests are allowed', 405
 
     request_json = request.get_json(silent=True)
+    
+    # Check for movie list POST request
+    if 'name' in request_json and 'media_list' in request_json:
+        return process_movie_list(request_json)
+    # Otherwise, assume it's a movie POST request
+    elif 'title' in request_json and 'ids' in request_json:
+        return process_movie(request_json)
+    else:
+        return 'Invalid POST request format', 400
 
-    if not request_json or not all(key in request_json for key in ['title', 'year', 'ids']):
+def process_movie(movie_data):
+    """Process a single movie object."""
+    if not all(key in movie_data for key in ['title', 'year', 'ids']):
         return 'Invalid movie object provided', 400
 
-    imdb_id = request_json['ids'].get('imdb')
+    imdb_id = movie_data['ids'].get('imdb')
     if not imdb_id:
         return 'No IMDb ID provided in movie object', 400
 
-    cleaned_data = remove_null_values(request_json)
-
+    cleaned_data = remove_null_values(movie_data)
     # Add movie to Firestore
     add_movie_to_firestore(db, imdb_id, cleaned_data)
     
     return f'Movie {imdb_id} added successfully!', 200
+
+def process_movie_list(movie_list_data):
+    """Process a movie list object."""
+    name = movie_list_data.get('name')
+    media_list = movie_list_data.get('media_list')
+
+    if not name or not media_list:
+        return 'Invalid movie list provided', 400
+    
+    # Add movie list to Firestore
+    add_movie_list_to_firestore(db, name, media_list)
+    return f"Movie list '{name}' added successfully!", 200
--- utils.py
+++ utils.py
@@ -12,7 +12,6 @@ def remove_null_values(d):
         return d
     return {k: remove_null_values(v) for k, v in d.items() if v is not None}
 
-
 def initialize_firestore():
     """Initialize and return a Firestore client."""
     return firestore.Client()
@@ -22,3 +21,10 @@ def add_movie_to_firestore(db, imdb_id, movie_data):
     movies_ref = db.collection('movies')
     movie_doc_ref = movies_ref.document(imdb_id)
     movie_doc_ref.set(movie_data, merge=True)
+
+def add_movie_list_to_firestore(db, name, media_list):
+    """Add a movie list to Firestore."""
+    movie_lists_ref = db.collection('movie-lists')
+    movie_lists_ref.document(name).set({
+        'media_list': media_list
+    })
